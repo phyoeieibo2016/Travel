9275ee24a414ced9af41d54aaf30e4fd6795be7e
phyoeiei.bo <phyoeieibo.peeb@gmail.com>
2020-09-09 14:45:46 +0630

    Routes!

--- lib/config/routs.dart
+++ lib/config/routs.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:travel_flutter/pages/main_page.dart';
 
-class Routes{
-static Map<String, WidgetBuilder> getRoutes(){
-  return <String, WidgetBuilder>{
+class Routes {
+  static Map<String, WidgetBuilder> getRoutes() {
+    return <String
+    , WidgetBuilder>{
     '/': (_) => MainPage();
-  };
-}
+    };
+  }
 }
